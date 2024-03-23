e92ef06a0adfafa56b15c375c3f9157a9d6dca83
Bruno Dias <brunodg.dev@gmail.com>
2024-03-23 12:22:39 -0300

    first commit

---
+++ Questions/q3.lua
@@ -0,0 +1,16 @@
+-- Fix or improve the name and the implementation of the method below
+
+-- This method is for the player to leave the party, so:
+
+--function do_sth_with_PlayerParty(playerId, membername)
+function leavePlayerParty(playerId, membername)
+    player = Player(playerId)
+    local party = player:getParty()
+
+    for k,v in pairs(party:getMembers()) do 
+        if v == Player(membername) then 
+            --party:removeMember(Player(membername))
+            party:removeMember(k) -- instead of removing the PlauerName, remove the key from the pair itself.
+        end
+    end
+end
