2025d4172275b0332e0bb9decf1797fd7997e67a
Bruno Dias <brunodg.dev@gmail.com>
2024-03-25 12:06:53 -0300

    [update] memory leak fixed in question 4

--- Questions/q4.cpp
+++ Questions/q4.cpp
@@ -3,22 +3,30 @@
     Fix the memory leak issue in below method.
 */
 
+/*
+    Changing Item* and Player* pointers to std::unique_ptr solves the memory leak in this function, because
+    in exception or early return of function, it prevents wrong memory allocation.
+*/
+
 void Game::addItemToPlayer(const std::string& recipient, uint16_t itemId)
 {
     Player* player = g_game.getPlayerByName(recipient);
-    if (!player) {
-        player = new Player(nullptr);
+    std::unique_ptr<Player> playerPtr;
+    if (!player)
+    {
+        playerPtr.reset(new Player(nullptr));
         if (!IOLoginData::loadPlayerByName(player, recipient)) {
             return;
         }
+        player = playerPtr.get();
     }
 
-    Item* item = Item::CreateItem(itemId);
+    std::unique_ptr<Item> item(Item::CreateItem(itemId));
     if (!item) {
         return;
     }
 
-    g_game.internalAddItem(player->getInbox(), item, INDEX_WHEREEVER, FLAG_NOLIMIT);
+    g_game.internalAddItem(player->getInbox(), item.get(), INDEX_WHEREEVER, FLAG_NOLIMIT);
 
     if (player->isOffline()) {
         IOLoginData::savePlayer(player);
